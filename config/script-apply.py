5fff32f7188bfc3e134c165da8c0b864b35028ce
Helen C S Santos <helen.santos@unico.io>
2025-04-21 20:52:40 -0300

    copy work remote

---
+++ config/script-apply.py
@@ -0,0 +1,37 @@
+import paramiko
+import os
+import time
+
+# Variáveis de ambiente
+switch_ip = os.environ.get('RUNNER_SWITCH_IP')
+tftp_server = os.environ.get('RUNNER_TFTP_SERVER')
+switch_username = os.environ.get('RUNNER_SWITCH_USERNAME')
+switch_password = os.environ.get('RUNNER_SWITCH_PASSWORD')
+port = '22'
+copy = 'copy tftp://' + tftp_server + '/poc-config running-config'
+
+# Autenticação SSH
+ssh = paramiko.SSHClient()
+ssh.load_system_host_keys()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(switch_ip, port, switch_username, switch_password)
+channel = ssh.invoke_shell()
+time.sleep(1)
+
+# Execução de comandos no Switch
+commands = [
+    'enable',
+    copy,
+    'y',
+    'write',
+    'y'
+]
+
+for command in commands:
+    channel.send (command + '\n')
+    time.sleep(10)
+    output = channel.recv(2048).decode()
+    print (output)
+
+channel.close()
+ssh.close()
